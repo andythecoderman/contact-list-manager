baaa1e154343c76980f80ccb9bcc1bd5deaf20cd
andythecoderman <andy.batchelor@tritondigital.com>
2015-03-24 13:01:07 -0400

    added sample-data.json and contacts route to stream contents on request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/contacts', function(req, res, next) {
+  //- Return List of customers from /data/sample-data.json
+    res.setHeader('Content-Type', 'application/json');
+    fs.createReadStream('data/sample-data.json').pipe(res);
 });
 
 module.exports = router;
